4dfe5a32f9787d14a837f8053a5f62895e54d370
Siddharth Gupta <siddharthguptawork@gmail.com>
2023-05-08 07:24:30 +0530

    changes for deployment

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-const mongoDB = "mongodb+srv://admin:mongo@cluster0.jldkogt.mongodb.net/ticketdb?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://admin:mongo@cluster0.jldkogt.mongodb.net/ticketdb?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch(err => console.log(err));
 async function main() {
